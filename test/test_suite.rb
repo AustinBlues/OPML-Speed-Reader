1939f99a4d539ec29366d440cae6ac66bd6131b5
Jeffrey L. Taylor <jeff.taylor@ieee.org>
2011-06-28 17:11:54 -0500

    Update testing to match current practice.

--- test/test_suite.rb
+++ test/test_suite.rb
@@ -10,22 +10,22 @@ class TestOpml < Test::Unit::TestCase
     Dir['./test/opml/*.yml'].each do |filename|
 #      puts "FILE: #{filename}."
       expected = YAML::load( File.open( filename ) )
+#      puts "EXPECTD: #{expected.inspect}."
       io = open(filename.gsub(/\.yml\Z/, '.xml'))
-      reader = XML::Reader.io(io)
-      opml = fetch(reader)
-#      puts "ACTUAL: #{opml[:feeds].inspect}."
+      opml = OpmlSpeedReader.parse(io)
+#      puts "ACTUAL:  #{opml.inspect}."
 #      puts "ACTUAL: #{opml[:feeds].map{|a| "'#{a.title}'"}.join(", ")}."
 #      puts "DETAILS: #{opml[:details].inspect}."
-      expected[:items].each do |item|
-#	puts "ITEM: |#{item.inspect}|"
-	item[:title].gsub!(/&#x([0-9a-f]+);/i){ [$1.hex].pack("U*")}
-	a = opml[:feeds].detect{|i| item[:title] == i['title']}
-#	a = opml.detect{|i| item[:title] == i.title}
+#      expected[:items].each do |item|
+      expected[:feeds].each do |feed|
+#	puts "FEED: |#{feed.inspect}|"
+	feed['title'].gsub!(/&#x([0-9a-f]+);/i){ [$1.hex].pack("U*")}
+	a = opml[:feeds].detect{|i| feed['title'] == i['title']}
 	if a.nil?
-	  puts "MISSING EXPECTED: '#{item[:title]}'."
+	  puts "MISSING EXPECTED: '#{feed['title']}'."
 	else
-	  assert item[:title] == a['title'], "ITEM TITLE: '#{a['title']}' vs. '#{item[:title]}'."
-	  assert a['url'] == item[:url], "ITEM URL: '#{a['url']}' vs. '#{item[:url]}'."
+	  assert feed['title'] == a['title'], "FEED TITLE: '#{a['title']}' vs. '#{feed['title']}'."
+	  assert a['url'] == feed['url'], "FEED URL: '#{a['url']}' vs. '#{feed['url']}'."
 
 	  opml[:feeds].delete(a)
 	end
@@ -33,26 +33,6 @@ class TestOpml < Test::Unit::TestCase
 #      puts "OPML: |#{opml[:feeds].inspect}|"
       assert(opml[:feeds].empty?,
 	     "#{opml[:feeds].length} channels expected in #{filename.gsub(%r{.*/}, '')} not found.")
-    end
-  end
-
-
-  def fetch(reader)
-    title = ''
-#    details = []
-    feeds = []
-    stack = []
-
-    begin
-      title = OpmlSpeedReader.parse_header(reader, stack)
-
-      stack.pop
-
-      OpmlSpeedReader.parse_body(reader, stack) do |libxml|
-	feeds << libxml
-      end
-
-      {:title => title, :feeds => feeds}
     end
   end
 end
