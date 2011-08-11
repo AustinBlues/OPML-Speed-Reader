2034bdc051c96e62250b8c255d08b91d9219c9ad
Jeffrey L. Taylor <jeff.taylor@ieee.org>
2011-08-11 09:22:41 -0500

    Improve handling of non-OPML files.

--- examples/opml_to_yaml.rb
+++ examples/opml_to_yaml.rb
@@ -3,4 +3,12 @@ require '../lib/opml_speed_reader'
 require 'yaml'
 
 
-puts OpmlSpeedReader.parse(STDIN).to_yaml
+# Disable noisy error reporting by libxml2 library on STDOUT
+XML::Error.set_handler(&XML::Error::QUIET_HANDLER)
+
+begin
+  puts OpmlSpeedReader.parse(STDIN).to_yaml
+rescue OpmlSpeedReader::NotOPML
+  STDERR.puts "Not OPML"
+  exit 1
+end
--- test/test_suite.rb
+++ test/test_suite.rb
@@ -6,6 +6,12 @@ require 'opml_speed_reader'
 
 
 class TestOpml < Test::Unit::TestCase
+  def setup
+    # Disable noisy error reporting by libxml2 library on STDOUT
+    XML::Error.set_handler(&XML::Error::QUIET_HANDLER)
+  end
+
+
   # Compare output for OPML files against expected (YAML test fixtures)
   def test_parsing
     Dir['./test/opml/*.yml'].each do |filename|
@@ -19,8 +25,6 @@ class TestOpml < Test::Unit::TestCase
   # Test exception thrown for non-OPML file (it's an HTML file, a
   # common user mistake.
   def test_not_opml
-    # Disable noisy error reporting by libxml2 library on STDOUT
-    XML::Error.set_handler(&XML::Error::QUIET_HANDLER)
     io = open('./test/opml/not_opml.html')
     assert_raise OpmlSpeedReader::NotOPML do
       OpmlSpeedReader.parse(io)
