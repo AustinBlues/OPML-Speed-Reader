62d8e94eb50e79220ee3b7889291383b2a0370f2
Jeffrey L. Taylor <jeff.taylor@ieee.org>
2011-08-30 19:06:51 -0500

    No non-OPML detection.

--- examples/opml_to_yaml.rb
+++ examples/opml_to_yaml.rb
@@ -6,10 +6,5 @@ require 'yaml'
 # Disable noisy error reporting by libxml2 library on STDOUT
 XML::Error.set_handler(&XML::Error::QUIET_HANDLER)
 
-begin
-  reader = XML::Reader.io(STDIN)
-  puts OpmlSpeedReader.parse(reader).to_yaml
-rescue OpmlSpeedReader::NotOPML
-  STDERR.puts "Not OPML"
-  exit 1
-end
+reader = XML::Reader.io(STDIN)
+puts OpmlSpeedReader.parse(reader).to_yaml
